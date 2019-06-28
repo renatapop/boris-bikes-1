7425461bc0a4448361fc1357e99bf3ee9fb7ceba
Student <student@Makerss-MacBook-Pro-2.local>
2019-06-28 16:03:05 +0100

    step 20 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,9 +7,11 @@ describe DockingStation do
   #expect {:release_bike}to raise_error("Whoops, no bike here!")
   # it { is_expected.to respond_to(:report_bike).with(1).argument}
 
+  let(:bike) { double :bike }
   it "docks something" do
-    bike = Bike.new
-    expect(subject.dock( double(:bike))[0]).to eq bike
+    bike = double(:bike)
+    allow(bike).to receive(:dock).and_return(:bike)
+    expect(subject.dock(bike)[0]).to eq bike
   end
   # it "returns docked bikes" do
   #   bike = Bike.new
@@ -23,11 +25,12 @@ describe DockingStation do
     it "Error message if no available bike" do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
+
+    # let(:bike) { double :bike }
     it 'does not release broken bikes' do
-      # bike = Bike.new
-      # bike.report_bike
-      subject.dock( double(:bike))
-      expect { subject.release_bike }.to raise_error 'No bikes available'
+      bike = double(:bike, broken?: true)
+      subject.dock bike
+      expect {subject.release_bike}.to raise_error 'No bikes available'
     end
   end
   describe '#dock' do
